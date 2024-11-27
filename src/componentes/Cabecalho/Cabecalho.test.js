68536e470406d7571729c8fb1cb514d5cf3d2b79
MichelMLeal <michellgt7@gmail.com>
2024-11-27 14:30:36 -0300

    adicionando mais testes

--- src/componentes/Cabecalho/Cabecalho.test.js
+++ src/componentes/Cabecalho/Cabecalho.test.js
@@ -6,3 +6,28 @@ test('Deve renderizar o nome do usuário logado', () => {
   const nomeUsuario = screen.getByText('Joana Fonseca Gomes');
   expect(nomeUsuario).toBeInTheDocument();
 });
+
+test('Deve renderizar o nome do usuário logado por propriedade', () => {
+  const nome = 'Michel';
+  render(<Cabecalho nomeUsuario={nome}/>);
+  const nomeUsuario = screen.getByText(nome);
+  expect(nomeUsuario).toBeInTheDocument();
+});
+
+test('Deve renderizar imagem do avatar', () => {
+  render(<Cabecalho />);
+  const textIcone = screen.getByAltText('Ícone de um avatar de usuário');
+  expect(textIcone).toBeInTheDocument();
+});
+
+test('renders Logo component', () => {
+  const { container } = render(<Cabecalho />);
+  const logoElement = container.querySelector('svg');
+  expect(logoElement).toBeInTheDocument();
+});
+
+test('Deve ter o nome do Logo', () => {
+  const { container } = render(<Cabecalho />);
+  const logo = container.querySelector('#logoByteBank');
+  expect(logo).toBeInTheDocument();
+});
